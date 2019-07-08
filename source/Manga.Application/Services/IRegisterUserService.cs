1a526fd6fb83c219e379b932bb4c4929a9f66852
Sahil Maiyani <sahilmaiyani@gmail.com>
2019-07-08 16:13:12 +0530

    wip: user registration through asp.net identity

--- source/Manga.Application/Services/IRegisterUserService.cs
+++ source/Manga.Application/Services/IRegisterUserService.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Manga.Application.Services
 {
-    interface IIdentityAuthService
+    public interface IRegisterUserService
     {
-
+        Task<Guid> Execute(string username, string password);
     }
 }
---
+++ source/Manga.Infrastructure/IdentityAuthentication/RegisterUser.cs
@@ -0,0 +1,33 @@
+﻿using Manga.Application.Services;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manga.Infrastructure.IdentityAuthentication
+{
+    sealed class RegisterUser : IRegisterUserService
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public Task<Guid> Execute(string username, string password)
+        {
+            return RegistrationAsync(username, password);
+        }
+
+        private async Task<Guid> RegistrationAsync(string username, string password)
+        {
+            var user = new IdentityUser { UserName = username };
+            var result = await userManager.CreateAsync(user, password);
+
+            if (result.Succeeded)
+            {
+                return new Guid(user.Id);
+            }
+
+            return new Guid();
+
+        }
+    }
+}
